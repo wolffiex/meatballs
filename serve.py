7724eebf13b8be852f4e4155da55e98e2c73b4f1
wolffiex <adam@sparko.local>
2024-01-01 13:23:11 -0800

    Unnest directory

--- serve.py
+++ serve.py
@@ -5,7 +5,7 @@ import http.server
 import socketserver
 
 PORT = 8099
-DIRECTORY = "www/html"
+DIRECTORY = "html"
 
 class ReusableAddressTCPServer(socketserver.TCPServer):
     allow_reuse_address = True
